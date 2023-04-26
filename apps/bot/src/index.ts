9414b25c957ad85a3829849ee6b4fa692e5be5e2
Nattamon Polvichai <joppy.pgh@gmail.com>
2023-04-26 18:25:53 +0700

    init stupid bot

---
+++ apps/bot/src/index.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import { Client, Events, GatewayIntentBits } from "discord.js";
+
+dotenv.config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
