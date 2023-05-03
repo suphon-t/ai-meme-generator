205b85bce8bc4d2ec8fa10403f9082a11aa4e459
Suphon Thanakornpakapong <paphonb@gmail.com>
2023-05-03 07:27:58 +0700

    unify two getmeme commands logic

--- apps/bot/src/commands/generic-meme.ts
+++ apps/bot/src/commands/generic-meme.ts
@@ -1,42 +1,17 @@
-import { BufferResolvable, ChatInputCommandInteraction } from "discord.js";
-import axios from "axios";
+import { ChatInputCommandInteraction } from "discord.js";
 import { Command } from "../command";
-import { GenericMemeIdea } from "../templates";
-import { download } from "../s3.js";
+import { TemplateId } from "../templates";
 import { captureException } from "@sentry/node";
+import { generateMeme } from "../generate-meme";
 
 export const genericMeme: Command = {
   name: "getgenericmeme",
   description: "Make a meme with no template from OpenAI and image search",
   run: async (interaction: ChatInputCommandInteraction) => {
     try {
-      // meme service
-      const memeBrainServiceResponse = await axios.post(
-        `${process.env.MEME_BRAIN_ENDPOINT}/generate_idea`,
-        {
-          templateId: "generic",
-        }
-      );
-      const memeIdea: GenericMemeIdea =
-        memeBrainServiceResponse.data.memeContent;
-      console.log(memeIdea);
-
-      // image service
-      const memeImageServiceResponse = await axios.post(
-        `${process.env.MEME_IMAGE_ENDPOINT}/generic_memes`,
-        {
-          picture: memeIdea.picture,
-          text0: memeIdea.text0,
-          text1: memeIdea.text1,
-        }
-      );
-      const memeImageFilename: string = memeImageServiceResponse.data.img[0];
-
-      // get image
-      console.log(memeImageFilename);
-      const memeImageBuffer = (await download(
-        memeImageFilename
-      )!) as BufferResolvable;
+      const memeImageBuffer = await generateMeme({
+        templateId: TemplateId.generic,
+      });
 
       await interaction.followUp({
         content: `${interaction.user.username}'s meme is served`,
---
+++ apps/bot/src/generate-meme.ts
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { GenericMemeIdea, MemeIdea, TemplateId } from "./templates";
+import { download } from "./s3";
+
+export interface GenerateMemeOptions {
+  templateId?: TemplateId;
+}
+
+export async function generateMeme(options: GenerateMemeOptions = {}) {
+  // meme service
+  const memeBrainServiceResponse = await axios.post(
+    `${process.env.MEME_BRAIN_ENDPOINT}/generate_idea`,
+    {
+      templateId: options.templateId,
+    }
+  );
+  const memeIdea: MemeIdea = memeBrainServiceResponse.data;
+  console.log(memeIdea);
+
+  // image service
+  if (memeIdea.templateId === undefined) {
+    throw new Error("meme brain returned undefined template id");
+  }
+
+  let memeImageFilename: string;
+  if (memeIdea.templateId === TemplateId.generic) {
+    const genericMemeIdea = memeIdea.memeContent as GenericMemeIdea;
+    const memeImageServiceResponse = await axios.post(
+      `${process.env.MEME_IMAGE_ENDPOINT}/generic_memes`,
+      {
+        picture: genericMemeIdea.picture,
+        text0: genericMemeIdea.text0,
+        text1: genericMemeIdea.text1,
+      }
+    );
+    memeImageFilename = memeImageServiceResponse.data.img[0];
+  } else {
+    const memeImageServiceResponse = await axios.post(
+      `${process.env.MEME_IMAGE_ENDPOINT}/caption_template`,
+      {
+        template: memeIdea.templateId,
+        text0: memeIdea.memeContent.text0,
+        text1: memeIdea.memeContent.text1,
+      }
+    );
+    memeImageFilename = memeImageServiceResponse.data.img;
+  }
+
+  // get image
+  console.log(memeImageFilename);
+  const memeImageBuffer = await download(memeImageFilename)!;
+  if (memeImageBuffer === undefined) {
+    throw new Error("meme-image returned an empty image");
+  }
+  return memeImageBuffer;
+}
